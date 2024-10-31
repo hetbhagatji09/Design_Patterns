7c41cd5f49621c5c2ab536362702a8ce37936417
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-01 00:49:34 +0530

    Modifies Circle in parentshape

--- src/Shapes/ParentShape.java
+++ src/Shapes/ParentShape.java
@@ -4,7 +4,7 @@ public class ParentShape {
     public static void main(String args[]){
         System.out.println("This is Circle");
 
-        Circle c=new Circle(20);
+        Circle c=new Circle(10,20,30);
         c.roll();
         System.out.println(c.area());
         Rectangle r=new Rectangle(10,20);
