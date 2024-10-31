52a4951a9b40f06781a8a5fb2f8412675b70bee2
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-01 00:32:32 +0530

    Made Main file for accesing parent shapes

---
+++ src/Shapes/ParentShape.java
@@ -0,0 +1,15 @@
+package Shapes;
+
+public class ParentShape {
+    public static void main(String args[]){
+        System.out.println("This is Circle");
+
+        Circle c=new Circle(20);
+        c.roll();
+        System.out.println(c.area());
+        Rectangle r=new Rectangle(10,20);
+        r.rotatable();
+        System.out.println(r.area());
+
+    }
+}
