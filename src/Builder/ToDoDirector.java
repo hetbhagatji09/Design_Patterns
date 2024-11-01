0ed8dd5d073fc3ba6282a4da8c929b74220b9046
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-01 18:13:55 +0530

    Add director class to handle procedure

---
+++ src/Builder/ToDoDirector.java
@@ -0,0 +1,28 @@
+package Builder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ToDoDirector {
+    List<ToDo>todos=new ArrayList<>();
+    public void addWithId(){
+        todos.add(
+                new ToDoBuilder().withId(1).build()
+        );
+    }
+    public void addAll(){
+        todos.add(
+                new ToDoBuilder().withId(2)
+                        .withTitle("Attendence")
+                        .withcompleted(true)
+                        .withDescreption("I am Going to Collage")
+                        .build()
+        );
+    }
+    public void printTodos(){
+        for(ToDo t:todos){
+            System.out.println(t);
+        }
+
+    }
+}
