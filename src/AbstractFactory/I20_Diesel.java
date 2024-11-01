876b90ec4318b3b6af249fe91e1d0947b13069d6
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-01 16:38:07 +0530

    created i20 car types

---
+++ src/AbstractFactory/I20_Diesel.java
@@ -0,0 +1,10 @@
+package AbstractFactory;
+
+public class I20_Diesel implements DieselCar{
+
+
+    @Override
+    public void start() {
+        System.out.println("Starting engine of I20 diesel");
+    }
+}
---
+++ src/AbstractFactory/I20_Petrol.java
@@ -0,0 +1,8 @@
+package AbstractFactory;
+
+public class I20_Petrol implements PetrolCar{
+    @Override
+    public void start() {
+        System.out.println("Starting engine of I20 petrol");
+    }
+}
