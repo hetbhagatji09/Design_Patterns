773919350480b6afb4bc36ca01f37569b902a9b1
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-01 11:54:34 +0530

    implements shape and overrides method

--- src/Factory/Rectangle.java
+++ src/Factory/Rectangle.java
@@ -1,6 +1,21 @@
 package Factory;
 
-public class Rectangle {
+public class Rectangle implements Shape{
     private int len;
     private int width;
+
+    public Rectangle(int len, int width) {
+        this.len = len;
+        this.width = width;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Draw the Rectangle");
+    }
+
+    @Override
+    public void fillColor() {
+
+    }
 }
