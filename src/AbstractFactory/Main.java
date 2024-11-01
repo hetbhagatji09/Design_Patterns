5b32eb60263ff615c003dd34108a58fd299f0e3d
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-01 16:42:31 +0530

    Main class for accessing showroom

---
+++ src/AbstractFactory/Main.java
@@ -0,0 +1,16 @@
+package AbstractFactory;
+
+public class Main {
+    public static void main(String args[]){
+        Showroom s1=new Showroom();
+        s1.addCar("I20","Petrol");
+        s1.addCar("Creta","Petrol");
+        s1.getCars();
+
+
+
+    }
+
+
+
+}
