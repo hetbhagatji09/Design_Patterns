2583fc0e28d51dd007d4edd4abc43b0599b6c659
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-01 12:11:38 +0530

    Add Getters and setters

--- src/Factory/Rectangle.java
+++ src/Factory/Rectangle.java
@@ -4,6 +4,22 @@ public class Rectangle implements Shape{
     private int len;
     private int width;
 
+    public int getLen() {
+        return len;
+    }
+
+    public void setLen(int len) {
+        this.len = len;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
     public Rectangle(int len, int width) {
         this.len = len;
         this.width = width;
