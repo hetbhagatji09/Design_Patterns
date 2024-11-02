20772dd43e719a8b2390d559d946af5eb6054b38
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-02 16:07:09 +0530

    create shape for rectangle and circle

---
+++ src/Prototype/Shape.java
@@ -0,0 +1,28 @@
+package Prototype;
+
+public abstract class Shape implements Cloneable {
+    private String id;
+    protected   String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    abstract void draw();
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+
+        return super.clone();
+    }
+}
