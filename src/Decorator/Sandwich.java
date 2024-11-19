e08e9c73a55653327ca3fd9f843afcf8dc0efe08
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-20 01:38:19 +0530

    print message for sadwich cost

--- src/Decorator/Sandwich.java
+++ src/Decorator/Sandwich.java
@@ -3,6 +3,7 @@ package Decorator;
 public class Sandwich implements BreadItem{
     @Override
     public int cost() {
+        System.out.println("Sandwich cost is 60");
         return 60;
     }
 }
