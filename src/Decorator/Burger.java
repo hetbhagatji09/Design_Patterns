24046a127d1cff00b0104d5f3c21bcb96b7418b8
hetbhagatji09 <hetbhagatji09@gmail.com>
2024-11-20 01:36:45 +0530

    print message in cost method

--- src/Decorator/Burger.java
+++ src/Decorator/Burger.java
@@ -3,6 +3,7 @@ package Decorator;
 public class Burger implements BreadItem{
     @Override
     public int cost() {
+        System.out.println("Burger's cost is 40");
         return 40;
     }
 }
